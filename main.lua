9286c6bd8bf2a1773fd1f346a92c636ec77d14a2
loremaster0 <cahaines@ymail.com>
2022-10-02 08:07:07 -0500

    Added LUA main flie

---
+++ main.lua
@@ -0,0 +1,27 @@
+-- file info
+
+-- file varibles
+
+-- @ "convert to" table
+
+-- @ "convert from" table
+
+-- @ "msgBus" string
+
+-- @ args varibles
+
+-- @@ "fileInput" file
+
+-- @@ "fileOutput" file
+
+-- @@ "msgInput" string
+
+-- file functions
+
+-- file main function
+
+-- file cli args options
+
+-- @ call main function based on cli args options
+
+-- EOF
